9577475e0eaddd897819c95b6c33e69c12e35eae
andrewthesailor <andrew.szczepanik@outlook.com>
2019-03-27 19:43:45 +0100

    may work need power

--- ball_mover_logic.py
+++ ball_mover_logic.py
@@ -1,43 +1,97 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, MediumMotor,OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1,INPUT_2,INPUT_3,INPUT_4
 from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.sensor.lego import ColorSensor
 from time import sleep
 
+
+zielony = (30, 90, 20)
+niebieski = (34, 75, 60)
+czerwony = (140, 30 , 10)
+
+granica = 40
+
 def find_starting_pod():
-    while get_color() != 1
-        follow_the_line()
-    
-    
+	while get_color(colSenMid) != 1:
+		follow_the_line()
+	if get_color(colSenRight) == 1:
+		print("right")
+		turn_right()
+		sleep(0.5)
+	else:
+		print("left")
+		turn_left()
+		sleep(0.5)
+	while get_color(colSenMid) not in (0,2):
+		print(get_color(colSenMid))
+		follow_the_line()
+	get_ball()
+
+
+	
+	
 
 
 def find_target_pod():
-    while get_color() != targetColor
-        follow_the_line()
-
-    open_arm()
-    targetColor = 0
-    get_out_of_pod()
+	global targetColor
+	while get_color(colSenMid) != targetColor:
+		follow_the_line()
+	if get_color(colSenRight) == targetColor:
+		print("right")
+		turn_right()
+		sleep(0.5)
+	else:
+		print("left")
+		turn_left()
+		sleep(0.5)
+	while get_color(colSenMid) not in (0,2):
+		print(get_color(colSenMid))
+		follow_the_line()
+	open_arm()
+	targetColor = 0
+	get_out_of_pod()
 
 
 def get_ball():
-    while inf.proximity()>0
-        go_fwd()
-    close_arm()
-    targetColor = get_color()
-    get_out_of_pod()
+	print("getting the ball")
+	while inf.proximity >= 1:
+		print(inf.proximity)
+		go_fwd()
+	print(inf.proximity)
+	print("catch")
+	close_arm()
+	global targetColor
+	targetColor = get_color(colSenMid)
+	get_out_of_pod()
 
 def get_out_of_pod():
-    tank_drive.on(turn_pow,-turn_pow)
-    sleep(2)
+	tank_drive.on(turn_pow,-turn_pow)
+	sleep(3)
+	print("job's done")
+	tank_drive.off()
+	while get_color(colSenMid) != 1:
+		follow_the_line()
 
-def is_pod():
+	while not is_both_black():
+		go_fwd()
 
+	turn_right()
+	sleep(0.5)
+	find_target_pod()
 
+def get_color(sensor):
+	kolor = sensor.rgb
 
+	if abs(kolor[0]-zielony[0])+abs(kolor[1]-zielony[1])+abs(kolor[2]-zielony[2]) < granica:
+		return 1
 
+	if abs(kolor[0]-czerwony[0])+abs(kolor[1]-czerwony[1])+abs(kolor[2]-czerwony[2]) < granica:
+		return 0
 
-def get_color():
+	if abs(kolor[0]-niebieski[0])+abs(kolor[1]-niebieski[1])+abs(kolor[2]-niebieski[2]) < granica:
+		return 2
+
+	return -1
 
 
 
@@ -51,12 +105,18 @@ def open_arm() :
 
 
 def is_line() :
-    if (colSenRight.reflected_light_intensity < 15) and (colSenLeft.reflected_light_intensity < 15) :
-        return True
-    if (colSenRight.reflected_light_intensity > 20) and (colSenLeft.reflected_light_intensity > 20) :
-    	return True
-    else :
-    	return False
+	if (colSenRight.reflected_light_intensity < 15) and (colSenLeft.reflected_light_intensity < 15) :
+		return True
+	if (colSenRight.reflected_light_intensity > 20) and (colSenLeft.reflected_light_intensity > 20) :
+		return True
+	else :
+		return False
+
+def is_both_black():
+	if (colSenRight.reflected_light_intensity < 15) and (colSenLeft.reflected_light_intensity < 15) :
+		return True
+	else:
+		return False
 
 def turn() :
 	if colSenRight.reflected_light_intensity < 15:
@@ -74,18 +134,15 @@ def turn_left():
 
 	tank_drive.on(-2-turn_pow,turn_pow+12)
 
-def stop():
-		tank_drive.on(0.1,0.1)
-
 def follow_the_line() :
 
-    if is_line() :
-    	#if colSenMid.reflected_light_intensity > 70:
-    	#	search_for_line()
-    	#else:
-        go_fwd()
-    else:
-    	turn() 
+	if is_line() :
+		#if colSenMid.reflected_light_intensity > 70:
+		#	search_for_line()
+		#else:
+		go_fwd()
+	else:
+		turn() 
 
 
 
@@ -93,7 +150,7 @@ def follow_the_line() :
 turn_pow=14
 fwd_pow=20
 
-
+mm = MediumMotor(OUTPUT_C)
 tank_drive = MoveTank(OUTPUT_A,OUTPUT_B)
 
 colSenRight = ColorSensor(INPUT_2)
@@ -106,3 +163,4 @@ targetColor = 0
 
 inf = InfraredSensor(INPUT_1)
 inf.mode = 'IR-PROX'
+find_starting_pod()
